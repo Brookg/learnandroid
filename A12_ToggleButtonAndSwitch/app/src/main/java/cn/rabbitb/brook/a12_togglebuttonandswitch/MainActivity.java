d8a01c28e9f6573cc171f3fc2eb8383d2d4d543b
RabbitBrook <gx10031@126.com>
2017-05-06 17:38:27 +0800

    新增项目
    A12_ToggleButtonAndSwitch
    A13_ProcessBar(未完成)

---
+++ A12_ToggleButtonAndSwitch/app/src/main/java/cn/rabbitb/brook/a12_togglebuttonandswitch/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.rabbitb.brook.a12_togglebuttonandswitch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
